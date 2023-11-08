65f73bf416370694c4030fcbd77609682c34490c
Paula Oseroff <oseroffp@gmail.com>
2023-11-07 22:01:43 -0300

    feat: add functions for R^2, adj R^2, sigma hat and coefficients variance

--- regression_metrics.py
+++ regression_metrics.py
@@ -26,4 +26,26 @@ def residual_standard_error(Y: np.ndarray, F: np.ndarray) -> float:
     n = Y.size
     mse = mean_square_error(Y, F)
     return math.sqrt(mse / (n - 2))
+
+def r_squared(Y: np.ndarray, F: np.ndarray) -> float:
+    sqr_err = square_error(Y, F)
+
+    mean_mtx = np.ones(Y.shape) * Y.mean() 
+    sqr_diff_mean = square_error(Y, mean_mtx)
+
+    return 1 - (sqr_err / sqr_diff_mean)
+
+def adj_r_squared(R_2: float, Y: np.ndarray, K: np.ndarray) -> float:
+    n = Y.size
+    p = K.size
+    return 1 - (((1 - R_2)*(n - 1)) / (n - p))
+
+def sigma_hat_sqr(Y: np.ndarray, K: np.ndarray, F: np.ndarray) -> float:
+    n = Y.size
+    p = K.size
+    return (1 / (n - p -1))*square_error(Y, F)
     
+def coefficients_variance(X: np.ndarray, sigma_hat_sqr: float) -> np.ndarray:
+    X_sqr = X.T.dot(X)
+    X_sqr_inv = np.linalg.inv(X_sqr)
+    return sigma_hat_sqr * X_sqr_inv
